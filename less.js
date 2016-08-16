36a73bd083f9434f9bb2e133102da9367d4ca39a
Kevin Kwok <antimatter15@gmail.com>
2016-08-15 19:03:46 -0700

    Update less.js

--- less.js
+++ less.js
@@ -12,9 +12,17 @@ module.exports = new CSSPluginBase(function compile(style, address, opts) {
   // use a file path in Node and a URL in the browser
   var filename = this.builder ? fromFileURL(address) : address;
 
+  var systemPlugin = {
+    install: function(less, pluginManager) {
+      pluginManager.addFileManager(makeFileManager(less, loader, address))
+    },
+    minVersion: [2, 1, 1]
+  }
+
   return System['import']('lesscss', module.id)
   .then(function(less) {
     return less.render(style, {
+      plugins: [systemPlugin],
       filename: filename,
       rootpath: (!opts.fileAsRoot || !loader.builder) && filename.replace(/[^/]+$/, ''),
       paths: opts.fileAsRoot && [filename.replace(/[^/]+$/, '')],
@@ -36,3 +44,33 @@ module.exports = new CSSPluginBase(function compile(style, address, opts) {
     };
   });
 });
+
+// based on webkit less-loader
+function makeFileManager(less, loader, address) {
+  function SystemFileManager(){ less.FileManager.apply(this, arguments) }
+  SystemFileManager.prototype = Object.create(less.FileManager.prototype);
+  SystemFileManager.prototype.supports = function(filename, currentDirectory, options, environment) {
+    // SystemFileManager handles all the files
+    return true;
+  };
+  SystemFileManager.prototype.supportsSync = function(filename, currentDirectory, options, environment) {
+    return false
+  };
+  SystemFileManager.prototype.loadFile = function(filename, currentDirectory, options, environment, callback) {
+    System['normalize'](filename, address).then(function(path){
+      return System['fetch']({ address: path, metadata: {} })
+    }).then(function(contents){
+      callback(null, {
+        contents: contents,
+        filename: filename
+      })  
+
+    }).catch(function(reason){
+      callback(reason, null)
+    })
+    
+  }
+  return new SystemFileManager()
+}
+
+
